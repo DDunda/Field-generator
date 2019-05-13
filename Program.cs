3645b69eb63d1f4c1e483c78e938a85441047fb5
DDunda <deaconfarrow.email@gmail.com>
2019-05-13 21:58:00 +1000

    Basic generated helper files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
