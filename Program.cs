c768f4fbcb41f77eacd8e96625bfb9602d10da17
DDunda <deaconfarrow.email@gmail.com>
2019-05-13 22:49:45 +1000

    Basic point force implementation

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using Tools;
 
-namespace Fields {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-    abstract class BasicPoint {
-        public Vector2 position;
-    }
+namespace Fields
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+	abstract class BasicPoint
+	{
+		public Vector2 position;
+		public double magnitude;
+		public abstract double GetForceMagnitude(Vector2 position2);
+		public abstract Vector2 GetVectorForce(Vector2 position2);
+	}
+
+	class InverseSquarePoint : BasicPoint
+	{
+		public InverseSquarePoint(Vector2 position, double magnitude)
+		{
+			this.position = position;
+			this.magnitude = magnitude;
+		}
+		public override double GetForceMagnitude(Vector2 position2)
+		{
+			return magnitude / (position - position2).sqrMagnitude;
+		}
+		public override Vector2 GetVectorForce(Vector2 position2)
+		{
+			return this.GetForceMagnitude(position2) * (this.position - position2).normalised;
+		}
+	}
 }
