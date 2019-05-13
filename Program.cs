ffc0895dce60ebe8771e9cbfd327858afd506c80
DDunda <deaconfarrow.email@gmail.com>
2019-05-13 22:44:23 +1000

    helper class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using Tools;
 
-namespace Fields
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Fields {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
+    }
+    abstract class BasicPoint {
+        public Vector2 position;
     }
 }
