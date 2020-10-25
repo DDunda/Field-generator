9b6f0753633f6940065174fe7326608f85c001d4
DDunda <deaconfarrow.email@gmail.com>
2020-10-25 23:54:05 +1100

    Github sucks
    This is what I committed last time I swear.

--- Fields/Program.cs
+++ Fields/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.Threading;
 using System.IO;
 using Tools;
+using Tools.EasyIO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Fields
 {
@@ -19,7 +23,6 @@ namespace Fields
 	{
 		public ImageSettings settings;
 		public BasicPoint[] points;
-		public Bitmap image;
 		public void NonStaticMain(object Oargs)
 		{
 			string[] args = (string[])Oargs;
@@ -41,6 +44,29 @@ namespace Fields
 				Save();
 		}
 
+		Vector2 GetForceAtPoint(Vector2 pos)
+        {
+			Vector2 ForceSum = Vector2.Zero;
+
+			foreach(BasicPoint point in points)
+				ForceSum += point.GetVectorForce(pos);
+
+			return ForceSum;
+        }
+
+		// Maps screen space to world space
+		Vector2 MapPoint(Vector2 screenPos)
+        {
+			Vector2 worldPos = screenPos - ((settings.imageSize - Vector2.One) / 2.0);
+			worldPos.y *= -1;
+			worldPos.x /= (settings.imageSize.x - 1);
+			worldPos.y /= (settings.imageSize.y - 1);
+			worldPos.x *= settings.windowSize.x;
+			worldPos.y *= settings.windowSize.y;
+			worldPos += settings.windowPosition;
+			return worldPos;
+		}
+
 		void renderAscii()
 		{
 
@@ -48,13 +74,102 @@ namespace Fields
 
 		void renderImage()
 		{
+			double minForce = 0;
+			double maxForce = 0;
 
+			UInt32 width = (UInt32)settings.imageSize.x;
+			UInt32 height = (UInt32)settings.imageSize.y;
+
+			Vector2[,] forcefield = new Vector2[width, height];
+
+			for (int y = 0; y < height; y++)
+				for (int x = 0; x < width; x++)
+				{
+					Vector2 pos = MapPoint(new Vector2(x, y));
+					Vector2 force = GetForceAtPoint(pos);
+
+					if (force.size < minForce || minForce == 0) minForce = force.size;
+					if (force.size > maxForce                 ) maxForce = force.size;
+
+					forcefield[x, y] = force;
+				}
+
+			if (settings.maxForce != 0) maxForce = settings.maxForce;
+
+			Bitmap image = new Bitmap((int)width, (int)height);
+
+			bool logScale = settings.logFactor != 0;
+
+			double a = settings.logFactor;
+			double logBase = (1 + a) / a;
+
+			for (int y = 0; y < height; y++)
+				for (int x = 0; x < width; x++)
+				{
+					Vector2 force = forcefield[x, y];
+					Vector2 forceDir = force.size == 0 || force.size >= maxForce ? Vector2.One / 2.0 : (force.normalised + Vector2.One) / 2.0;
+
+					if (force.size > maxForce) force = force.normalised * maxForce;
+
+					double scaledForce = (force.size - minForce) / (maxForce - minForce);
+					if(logScale)
+						scaledForce = Math.Log((scaledForce + a) / a, logBase);
+
+					int R = (int)Math.Round(255 * forceDir.x);
+					int G = (int)Math.Round(255 * forceDir.y);
+					int B = (int)Math.Round(255 * scaledForce);
+
+					image.SetPixel(x, y, Color.FromArgb(255, R, G, B));
+                }
+			if (!Directory.Exists(settings.saveLocation)) Directory.CreateDirectory(settings.saveLocation);
+
+			string outputPath = $@"{settings.saveLocation}\Output.png";
+			image.Save(outputPath, ImageFormat.Png);
 		}
 
 		void Load()
 		{
-			string[] output = EasyIO.Read(settings.pointSource);
+			if (!File.Exists(settings.pointSource)) throw new FileNotFoundException($"\"{settings.pointSource}\" does not exist!");
+			string[] output = EasyIO.Read(settings.pointSource).ToArray();
+			string type = "";
+			List<string> args = new List<string>();
+			List<BasicPoint> newPoints = new List<BasicPoint>();
 
+			BasicPoint point;
+
+			foreach (string line in output)
+            {
+				if(BasicPoint.pointTypes.ContainsKey(line.Trim()))
+                {
+					if(type != "")
+                    {
+						point = null;
+						switch(type)
+                        {
+							case "InverseSquarePoint":
+								point = new InverseSquarePoint().ParseSaveData(args.ToArray());
+									break;
+						}
+						if(point != null) newPoints.Add(point);
+
+						args.Clear();
+						type = "";
+                    }
+					type = line.Trim();
+                }
+				else args.Add(line);
+            }
+
+			point = null;
+			switch (type)
+			{
+				case "InverseSquarePoint":
+					point = new InverseSquarePoint().ParseSaveData(args.ToArray());
+					break;
+			}
+			if (point != null) newPoints.Add(point);
+
+			points = newPoints.ToArray();
 		}
 
 		void Save()
@@ -68,7 +183,7 @@ namespace Fields
 			if (Output.Count > 0)
 				Output.RemoveAt(Output.Count - 1); // Removes the trailing empty line
 
-			EasyIO.Write(Output.ToArray(), settings.pointSource);
+			EasyIO.Write(settings.pointSource, Output.ToArray());
 		}
 	}
 
@@ -79,14 +194,16 @@ namespace Fields
 			Image, Ascii
 		}
 		static string[] shortArgumentExceptions = { "source" }; // Arguments that don't need two components
-		public Vector2 windowSize = new Vector2(50, 50); // Size in pixels or characters (w,h)
+		public Vector2 imageSize = new Vector2(50, 50); // Size in pixels or characters (w,h)
 		public Vector2 windowPosition = Vector2.Zero; // Where the center is
-		public Vector2 windowDilation = Vector2.One; // How the output is stretched
-		public RenderMode drawMode = RenderMode.Ascii; // How the output is rendered
+		public Vector2 windowSize = Vector2.One; // Size of window in screenspace
+		public RenderMode drawMode = RenderMode.Image; // How the output is rendered
 		public string saveLocation = Directory.GetCurrentDirectory().ToString() + @"\Default"; // Where output is saved
 		public string pointSource = ""; // Full directory of a save file to read from or write to
 		public bool addPoints = false; // Whether or not to add points to the world from the console
-		public bool saveOnExit = true; // Whether or not to save the world to the pointSource save file
+		public bool saveOnExit = false; // Whether or not to save the world to the pointSource save file
+		public double maxForce = 0;
+		public double logFactor = 0;
 		public static ImageSettings Parse(string[] args)
 		{
 			ImageSettings newSettings = new ImageSettings();
@@ -96,23 +213,23 @@ namespace Fields
 				if (argBits.Length != 2 && Array.IndexOf(shortArgumentExceptions, argBits[0]) == -1) throw new ArgumentException($"Incomplete argument ({arg})");
 				switch (argBits[0])
 				{
+					case "imageWidth":
+						newSettings.imageSize.x = Convert.ToUInt32(argBits[1]);
+						break;
+					case "imageHeight":
+						newSettings.imageSize.y = Convert.ToUInt32(argBits[1]);
+						break;
 					case "x":
 						newSettings.windowPosition.x = Convert.ToDouble(argBits[1]);
 						break;
 					case "y":
 						newSettings.windowPosition.y = Convert.ToDouble(argBits[1]);
 						break;
-					case "width":
-						newSettings.windowSize.x = Convert.ToUInt32(argBits[1]);
+					case "windowWidth":
+						newSettings.windowSize.x = Convert.ToDouble(argBits[1]);
 						break;
-					case "height":
-						newSettings.windowSize.y = Convert.ToUInt32(argBits[1]);
-						break;
-					case "xScale":
-						newSettings.windowDilation.x = Convert.ToDouble(argBits[1]);
-						break;
-					case "yScale":
-						newSettings.windowDilation.y = Convert.ToDouble(argBits[1]);
+					case "windowHeight":
+						newSettings.windowSize.y = Convert.ToDouble(argBits[1]);
 						break;
 					case "mode":
 						newSettings.drawMode = (RenderMode)Convert.ToUInt32(argBits[1]);
@@ -129,6 +246,12 @@ namespace Fields
 						break;
 					case "add":
 						newSettings.addPoints = Convert.ToBoolean(argBits[1]);
+						break;
+					case "maxForce":
+						newSettings.maxForce = Convert.ToDouble(argBits[1]);
+						break;
+					case "logFactor":
+						newSettings.logFactor = Convert.ToDouble(argBits[1]);
 						break;
 					default:
 						throw new ArgumentException($"\"{argBits[0]}\" is not a valid setting.");
@@ -232,10 +355,12 @@ namespace Fields
 		}
 		public override double GetForceMagnitude(Vector2 position2)
 		{
+			if ((position - position2).sqrSize == 0) return 0;
 			return constant / (position - position2).sqrSize;
 		}
 		public override Vector2 GetVectorForce(Vector2 position2)
 		{
+			if((this.position - position2).size == 0) return Vector2.Zero;
 			return (this.position - position2).normalised * this.GetForceMagnitude(position2);
 		}
 		public override BasicPoint ParseSaveData(string[] args)
@@ -243,7 +368,7 @@ namespace Fields
 			InverseSquarePoint newPoint = new InverseSquarePoint();
 			foreach (string line in args)
 			{
-				string[] components = line.Trim().Split(' ');
+				string[] components = Regex.Replace(line.Trim(), @"\s+", " ").Split(' ');
 				switch (components[0])
 				{
 					case "position":
@@ -262,7 +387,7 @@ namespace Fields
 		public override string[] GetSaveData()
 		{
 			return new string[] {
-				"ElasticPoint",
+				"InverseSquarePoint",
 				$"  position {this.position.x} {this.position.y}",
 				$"  constant {this.constant}",
 				""
